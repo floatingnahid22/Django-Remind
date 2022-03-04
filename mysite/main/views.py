b4d3df80e645b0ea6c6afd300d26438e00c18b75
floatingnahid22 <nahidhossain>
2022-03-04 14:03:03 +0600

    edit apps views and urls.py file

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+def index(response, name):
+    ls  = ToDoList.objects.get(name=name)
+    item= ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br<</br><p>%s</s>" %(ls.name,str(item.text)))
 
