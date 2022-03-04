46ffc157a85269468af39eaec4c43d63e7a4d3c0
floatingnahid22 <nahidhossain>
2022-03-04 22:34:44 +0600

    update list and views of main app

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,8 +5,6 @@ from .models import ToDoList, Item
 # Create your views here.
 
 def index(response, id):
-
-    
     ls  = ToDoList.objects.get(id=id)
     return render(response, "main/list.html", {"ls":ls})
 
