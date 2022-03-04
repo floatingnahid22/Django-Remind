c6889443b56c111e83c84c8e1a7861cee54f5179
floatingnahid22 <nahidhossain>
2022-03-05 03:45:37 +0600

    update views.py of main apss for not logged in users redirect then to home page

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -8,24 +8,26 @@ from .forms import CreateNewList
 def index(response, id):
     ls  = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c"+str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c"+str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()    
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                item.save()    
-
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-    return render(response, "main/list.html", {"ls":ls})
+                    print("invalid")
+        return render(response, "main/list.html", {"ls":ls})
+    return render(response, "main/home.html", {})    
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -43,3 +45,7 @@ def create(response):
     else:        
         form  = CreateNewList()
     return render(response, "main/create.html", {"form":form})
+
+
+def view(response):
+    return render(response, "main/view.html", {})
