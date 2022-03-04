654d3e78b633de202191995af9aed0495a84e583
floatingnahid22 <nahidhossain>
2022-03-04 17:00:59 +0600

    rendering base.html

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,10 +4,9 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, name):
-    ls  = ToDoList.objects.get(name=name)
-    item= ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br<</br><p>%s</s>" %(ls.name,str(item.text)))
+def index(response, id):
+    ls  = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {})
 
 def home(response):
-    pass
+    return render(response, "main/home.html", {})
